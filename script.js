253a3b763d62925c017d38aa0d1c342108bee3a1
Mustafizur Rahaman <61288061+MustfizurRahaman@users.noreply.github.com>
2024-01-24 00:10:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var map = L.map('map').setView([37.7, -122.4], 10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+  // load GeoJSON from an external file
+var ratIcon = L.icon({
+    iconUrl: 'http://maptimeboston.github.io/leaflet-intro/rat.gif',
+    iconSize: [50, 40]
+});
+
+$.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson", function(data) {
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {icon: ratIcon});
+        }
+    }).addTo(map);
+});
